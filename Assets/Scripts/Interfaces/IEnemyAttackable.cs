d68f3645737dc4ddce047e4ea3d96514ed1a1283
PepijnL <pepijn.luchtmeijer@gmail.com>
2025-01-17 12:39:49 +0100

    Minor cleanup

--- Assets/Scripts/Interfaces/IEnemyAttackable.cs
+++ Assets/Scripts/Interfaces/IEnemyAttackable.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public interface IEnemyAttackable
 {
-    public GameObject GetAttackingAgent();
-    public Transform GetTransform();
-    public bool GetIsBeingAttacked();
-    public float GetHealth();
-    public void SetHealth(float _health);
-    public void SetIsInDanger(bool _isInDanger);
-    public void SetIsBeingAttacked(bool _isBeingAttacked);
-    public void SetAttackingAgent(GameObject _agent, bool _add);
+    GameObject GetAttackingAgent();
+    Transform GetTransform();
+    bool GetIsBeingAttacked();
+    float GetHealth();
+    void SetHealth(float _health);
+    void SetIsInDanger(bool _isInDanger);
+    void SetIsBeingAttacked(bool _isBeingAttacked);
+    void SetAttackingAgent(GameObject _agent, bool _add);
 }
